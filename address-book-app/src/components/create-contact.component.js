280436fbcc0c6b4d9936f2f7f91517bb82700760
WebGem Technologies <portfolio@pratikmore.com>
2022-06-16 09:18:19 -0400

    Updated Repo
    Created React application
    Installed Bootstrap
    Initialized CRUD operation
    Form, add, edit file completed

---
+++ address-book-app/src/components/create-contact.component.js
@@ -0,0 +1,38 @@
+// Creating a component for creating a contact or adding a contact
+
+// Import Modules
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import ContactForm from "./ContactForm";
+
+// CreateContact Component
+const CreateContact = () => {
+const [formValues, setFormValues] =
+	useState({ name: '', email: '', mobile: '' })
+
+// onSubmit handler
+const onSubmit = contactObject => {
+	axios.post(
+'http://localhost:4000/contacts/create-contact',
+	contactObject)
+	.then(res => {
+		if (res.status === 200)
+		alert('Contact successfully created')
+		else
+		Promise.reject()
+	})
+	.catch(err => alert('Something went wrong'))
+}
+	
+// Return contact form
+return(
+	<ContactForm initialValues={formValues}
+	onSubmit={onSubmit}
+	enableReinitialize>
+	Create Contact
+	</ContactForm>
+)
+}
+
+// Export CreateContact Component
+export default CreateContact
---
+++ address-book-app/src/components/edit-contact.component.js
@@ -0,0 +1,60 @@
+// EditContact Component for update contact data
+
+// Import Modules
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import ContactForm from "./ContactForm";
+
+// EditContact Component
+const EditContact = (props) => {
+const [formValues, setFormValues] = useState({
+	name: "",
+	email: "",
+	mobile: "",
+});
+	
+//onSubmit handler
+const onSubmit = (contactObject) => {
+	axios
+	.put(
+		"http://localhost:4000/contacts/update-contact/" +
+		props.match.params.id,
+		contactObject
+	)
+	.then((res) => {
+		if (res.status === 200) {
+		alert("Contact successfully updated");
+		props.history.push("/contact-list");
+		} else Promise.reject();
+	})
+	.catch((err) => alert("Something went wrong"));
+};
+
+// Load data from server and reinitialize contact form
+useEffect(() => {
+	axios
+	.get(
+		"http://localhost:4000/contacts/update-contacts/"
+		+ props.match.params.id
+	)
+	.then((res) => {
+		const { name, email, mobile } = res.data;
+		setFormValues({ name, email, mobile });
+	})
+	.catch((err) => console.log(err));
+}, []);
+
+// Return contact form
+return (
+	<ContactForm
+	initialValues={formValues}
+	onSubmit={onSubmit}
+	enableReinitialize
+	>
+	Update Contact
+	</ContactForm>
+);
+};
+
+// Export EditContact Component
+export default EditContact;

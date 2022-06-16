de1e3732aa5658e97ead602dc2925191e72190f1
Pratik More <portfolio@pratikmore.com>
2022-06-16 10:45:15 -0400

    backup file
    Removed accidently so I have used backup as working on 2 sides taking backups to avoid mishaps.

---
+++ address-book-app/src/components/ContactTableRow.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const ContactTableRow = (props) => {
+const { _id, pic, firstname, lastname, email, rollno } = props.obj;
+
+const deleteContact = () => {
+	axios
+	.delete(
+"http://localhost:4000/contacts/delete-contact/" + _id)
+	.then((res) => {
+		if (res.status === 200) {
+		alert("Contact successfully deleted");
+		window.location.reload();
+		} else Promise.reject();
+	})
+	.catch((err) => alert("Something went wrong"));
+};
+
+return (
+	<tr>
+	<td>{pic}</td>
+    <td>{firstname}</td>
+    <td>{lastname}</td>
+	<td>{email}</td>
+	<td>{rollno}</td>
+	<td>
+		<Link className="edit-link"
+		to={"/edit-contact/" + _id}>
+		Edit
+		</Link>
+		<Button onClick={deleteContact}
+		size="sm" variant="danger">
+		Delete
+		</Button>
+	</td>
+	</tr>
+);
+};
+
+export default ContactTableRow;
---
+++ address-book-app/src/components/contact-list.component.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Table } from "react-bootstrap";
+import ContactTableRow from "./ContactTableRow";
+
+const ContactList = () => {
+const [contacts, setContacts] = useState([]);
+
+useEffect(() => {
+	axios
+	.get("http://localhost:4000/contacts/")
+	.then(({ data }) => {
+		setContacts(data);
+	})
+	.catch((error) => {
+		console.log(error);
+	});
+}, []);
+
+const DataTable = () => {
+	return contacts.map((res, i) => {
+	return <ContactTableRow obj={res} key={i} />;
+	});
+};
+
+return (
+	<div className="table-wrapper">
+	<Table striped bordered hover>
+		<thead>
+		<tr>
+			<th>Name</th>
+			<th>Email</th>
+			<th>Roll No</th>
+			<th>Action</th>
+		</tr>
+		</thead>
+		<tbody>{DataTable()}</tbody>
+	</Table>
+	</div>
+);
+};
+
+export default ContactList;
